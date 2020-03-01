2dbb1f905d5e53b7ec692e4d9209c4c8f6930ff3
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-03-01 22:54:49 +0530

    Added miles per hr conversion app

---
+++ miles-per-hr conversion.py
@@ -0,0 +1,4 @@
+print("Welcome to the MPH to MPS Conversion App")
+mph = float(input("What is your speed in miles per hour?"))
+mps = mph * 0.44704
+print("Your speed in metres in per second is:", mps)
